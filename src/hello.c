ec479ad7dc1d76a2ca74ccb821b2ab83e9b3c666
Adil Yuksel <yukseladil1@gmail.com>
2026-09-14 10:24:30 +0000

    uptaed message

--- src/hello.c
+++ src/hello.c
@@ -1,8 +1,8 @@
 
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-  // This is your first C program my friend
+int main(int argc, char* argv[]) {
+  printf("Hello from <Adil>! \n");
   printf("Hello, RTU World from C Lab in 2025!\n");
   printf("You passed %d argument(s).\n", argc - 1);
   for (int i = 1; i < argc; ++i) {
